0b26a727c7ba92df9995a39194600d0b4e4d7b73
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2021-07-09 17:07:05 +0200

    chore: initialize

---
+++ scripts/openapi-generator-cli.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run \
+  --rm \
+  --workdir "/app" \
+  --volume "$(pwd):/app" \
+  --user "$(id -u):$(id -g)" \
+  openapitools/openapi-generator-cli generate \
+    --generator-name typescript-fetch \
+    --config openapi-config.yaml \
+    --input-spec schema.yml
+    --output src/api
